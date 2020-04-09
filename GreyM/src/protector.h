c68f84f5cdb71f07d040673ac2ec2f9e1e14ee98
greyb1t <gresan6@hotmail.com>
2020-04-09 23:24:11 +0200

    Added TLS section name in header

--- GreyM/src/protector.h
+++ GreyM/src/protector.h
@@ -1,7 +1,8 @@
 #pragma once
 
-#define VM_LOADER_SECTION_NAME (".dick")
-#define VM_CODE_SECTION_NAME (".dick2")
+#define VM_LOADER_SECTION_NAME ( ".dick" )
+#define VM_CODE_SECTION_NAME ( ".dick2" )
+#define TLSBABY_SECTION_NAME ( ".tlsbaby" )
 
 class PortableExecutable;
 
